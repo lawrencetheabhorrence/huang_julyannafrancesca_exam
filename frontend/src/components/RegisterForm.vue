ad5bdb31e52c81e92918ad76d321a8e179cad789
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-08-21 16:11:00 +0800

    initial registration form

---
+++ frontend/src/components/RegisterForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <q-form class="q-pa-md column items-stretch q-gutter-sm" style="width: 100%;">
+      <div class="row q-gutter-sm">
+        <q-input class="col" outlined label="Full Name" />
+        <q-input class="col" outlined label="Email" />
+      </div>
+      <div class="row q-gutter-sm">
+        <q-input class="col" outlined label="Password" />
+        <q-input class="col" outlined label="Confirm Password" />
+      </div>
+      <q-btn class="q-py-md" label="REGISTER" filled style="align-self: center; background: #8b3f93; color: white; width: 250px;" />
+    </q-form>
+</template>
