b1d7310d7e6c7ca11341c5d79e0f9cfa32cbf798
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-08-21 17:33:25 +0800

    initial attributes and relations for product and product order

--- backend/app/Models/ProductOrder.php
+++ backend/app/Models/ProductOrder.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ProductOrder extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductOrderFactory> */
     use HasFactory;
+
+    public function customer(): BelongsTo {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function product(): HasOne {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
 }
--- backend/database/migrations/2025_08_21_092632_create_products_table.php
+++ backend/database/migrations/2025_08_21_092632_create_products_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('image');
+            $table->float('price');
+            $table->integer('stock');
         });
     }
 
