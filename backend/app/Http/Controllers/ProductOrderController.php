7d904e1991776b264a01079573636b8bb54ee160
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-08-21 17:26:56 +0800

    initial product and productorder models

---
+++ backend/app/Http/Controllers/ProductOrderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductOrderRequest;
+use App\Http\Requests\UpdateProductOrderRequest;
+use App\Models\ProductOrder;
+
+class ProductOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductOrderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductOrder $productOrder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductOrder $productOrder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductOrderRequest $request, ProductOrder $productOrder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductOrder $productOrder)
+    {
+        //
+    }
+}
