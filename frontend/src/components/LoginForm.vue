fe19f51f5abe43d782941ae3942c6eee86af577f
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-08-21 15:53:35 +0800

    login form

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,7 @@
+<template>
+    <q-form class="column items-stretch q-gutter-sm" style="width: 400px;">
+      <q-input outlined stack-label label="Email" />
+      <q-input outlined stack-label label="Password" />
+      <q-btn flat style="background: #8b3f93; color: white;" label="LOGIN" />
+    </q-form>
+</template>
