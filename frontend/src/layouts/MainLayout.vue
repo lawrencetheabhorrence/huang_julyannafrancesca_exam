01eb9db2d8799a702bb3bd361438de89fe774fb8
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-08-22 23:14:23 +0800

    logo and drawer for admin cms

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="q-pa-md column items-center q-gutter-sm" style="max-width: 800px;">
-    <img src="logo.png" style="max-width: 300px;" />
-    <q-form class="q-pa-md column items-stretch q-gutter-sm" style="width: 100%;">
-      <div class="row q-gutter-sm">
-        <q-input class="col" outlined label="Full Name" />
-        <q-input class="col" outlined label="Email" />
-      </div>
-      <div class="row q-gutter-sm">
-        <q-input class="col" outlined label="Password" />
-        <q-input class="col" outlined label="Confirm Password" />
-      </div>
-      <q-btn class="q-py-md" label="REGISTER" filled style="align-self: center; background: #8b3f93; color: white; width: 250px;" />
-    </q-form>
-  </div>
-</template>
+  <q-layout view="hHh lpR fFf">
 
+    <q-drawer class="column items-stretch justify-between q-py-lg" show-if-above v-model="leftDrawerOpen" side="left" style="width: 100%;" elevated>
+      <section class="column items-center">
+        <img src="/logo.png" style="width: 90%; padding: 10px 30px; border-bottom: 1px solid grey;">
+        <q-list class="column items-center q-pt-md" style="width: 100%;">
+          <q-item class="item" style="width: 100%;" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="black" name="inbox" />
+            </q-item-section>
+            <q-item-section>Products Management</q-item-section>
+          </q-item>
+          <q-item class="item" style="width: 100%;" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="black" name="inbox" />
+            </q-item-section>
+            <q-item-section>Orders</q-item-section>
+          </q-item>
+          <q-item class="item" style="width: 100%;" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon color="black" name="group" />
+            </q-item-section>
+            <q-item-section>Users Management</q-item-section>
+          </q-item>
+        </q-list>
+      </section>
+      <section class="row items-center justify-between q-px-md" style="width: 100%;">
+        <section class="row items-center">
+          <q-avatar size="48px" font-size="32px" text-color="purple" icon="person" style="background: #eaddff;"/>
+          <p class="q-pl-md text-body2" style="margin: 0;">Hi, <span class="text-weight-medium text-body1">Admin!</span></p>
+        </section>
+        <q-btn flat rounded color="red" icon="logout" style="justify-self: end;" />
+      </section>
+    </q-drawer>
+
+    <q-page-container>
+    </q-page-container>
+
+  </q-layout>
+</template>
